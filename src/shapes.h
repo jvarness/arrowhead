05f9d2559cd7b80a043080379775f3bec5a584b8
Jake Varness <jake.varness@gmail.com>
2016-02-06 16:19:14 -0600

    Added an hour indicator and default colors

--- src/shapes.h
+++ src/shapes.h
@@ -4,17 +4,14 @@
 static const GPathInfo TRIANGLE_PATH = {
   .num_points = 3,
   .points = (GPoint[]) {{0,-10},{10,10},{-10,10}}
-  //.points = (GPoint[]) {{10,20},{20,0},{0,0}}
 };
 
 static const GPathInfo ARROWHEAD_PATH = {
   .num_points = 4,
   .points = (GPoint[]) {{0,-10},{10,10},{0,5},{-10,10}}
-  //.points = (GPoint[]) {{10,20},{20,0},{10,5},{0,0}}
 };
 
 static const GPathInfo BATTERY_IND = {
   .num_points = 4,
   .points = (GPoint[]) {{0,-15}, {10,0}, {0,15}, {-10,0}}
-  //.points = (GPoint[]) {{10,0}, {20,15}, {10,30}, {0,15}}
 };
